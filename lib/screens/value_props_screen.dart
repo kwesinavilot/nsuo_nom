8cfb744d6ac0f7af39109f35d167bd687c44280c
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-01-24 16:26:08 +0000

    Removed the previous slider and used introduction_screen instead

--- lib/screens/value_props_screen.dart
+++ lib/screens/value_props_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class ValueProps extends StatelessWidget {
   const ValueProps({super.key});
@@ -7,91 +7,66 @@ class ValueProps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: OnBoardingSlider(
-        headerBackgroundColor: Colors.red,
-        finishButtonText: 'Continue',
-        finishButtonStyle: const FinishButtonStyle(
-          backgroundColor: Colors.blueAccent,
+        body: IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
+          ),
         ),
-        background: [
-          Image.asset('assets/woman_drinking.gif'),
-          Image.asset('assets/sman_drinking.gif'),
-          Image.asset('assets/man_drinking.gif'),
-        ],
-        totalPage: 3,
-        speed: 1.8,
-        pageBodies: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: const Column(
-              children: [
-                Text(
-                  "Effortless Hydration Tracking",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "Easily record how much water you drink throughout the day with a tap of a button. No more manual tracking.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: const Column(
-              children: [
-                Text(
-                  "Effortless Hydration Tracking",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
+        ),
+        PageViewModel(
+          title: "Title of introduction page",
+          body: "Welcome to the app! This is a description of how it works.",
+          image: const Center(
+            child: Icon(Icons.waving_hand, size: 50.0),
           ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: const Column(
-              children: [
-                Text(
-                  "Effortless Hydration Tracking",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-
-      // body: Container(
-      //     color: Colors.red,
-      //     alignment: Alignment.center,
-      //     child: const Column(
-      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //       children: [
-      //         Text(
-      //           "Effortless Hydration Tracking",
-      //           style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-      //         ),
-      //         // SizedBox(height: 20), //put a space between the elems before and after
-      //         Image(
-      //           image: AssetImage('assets/woman_drinking.gif'),
-      //           fit: BoxFit.contain,
-      //           height: 350,
-      //           width: 350,
-      //         ),
-      //         // SizedBox(height: 30), //put a space between the elems before and after
-      //         Text(
-      //           "Easily record how much water you drink throughout the day with a tap of a button. No more manual tracking.",
-      //           textAlign: TextAlign.center,
-      //           style: TextStyle(
-      //             fontSize: 18,
-      //           ),
-      //         )
-      //       ],
-      //     )
-      //   ),
-    );
+        )
+      ],
+      showSkipButton: true,
+      showNextButton: false,
+      skip: const Text("Skip"),
+      done: const Text("Done"),
+      onDone: () {
+        // On button pressed
+      },
+    )
+        // body: Container(
+        //     color: Colors.red,
+        //     alignment: Alignment.center,
+        //     child: const Column(
+        //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //       children: [
+        //         Text(
+        //           "Effortless Hydration Tracking",
+        //           style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        //         ),
+        //         // SizedBox(height: 20), //put a space between the elems before and after
+        //         Image(
+        //           image: AssetImage('assets/woman_drinking.gif'),
+        //           fit: BoxFit.contain,
+        //           height: 350,
+        //           width: 350,
+        //         ),
+        //         // SizedBox(height: 30), //put a space between the elems before and after
+        //         Text(
+        //           "Easily record how much water you drink throughout the day with a tap of a button. No more manual tracking.",
+        //           textAlign: TextAlign.center,
+        //           style: TextStyle(
+        //             fontSize: 18,
+        //           ),
+        //         )
+        //       ],
+        //     )
+        // ),
+        );
   }
 }
