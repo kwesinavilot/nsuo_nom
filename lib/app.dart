d08a9ab42e69658717bf365cc7c6a6223b47cc78
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-01-24 03:44:51 +0000

    Brought the MaterialApp over into runApp in main.dart

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nsuonum',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:nsuo_nom/app.dart';
+import 'package:nsuo_nom/screens/homescreen.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    MaterialApp(
+      title: 'Nsuonum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(title: 'Nsuonom'),
+    ),
+  );
 }
