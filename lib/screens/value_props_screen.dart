4f91b59d922869ab14782c34e90309755e3eba7b
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-01-24 15:17:48 +0000

    Added gifs and incorporated them

--- lib/screens/value_props_screen.dart
+++ lib/screens/value_props_screen.dart
@@ -16,7 +16,16 @@ class ValueProps extends StatelessWidget {
                 "Effortless Hydration Tracking",
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
-
+              SizedBox(
+                  height: 20), //put a space between the elems before and after
+              Image(
+                image: AssetImage('assets/woman_drinking.gif'),
+                fit: BoxFit.contain,
+                height: 350,
+                width: 350,
+              ),
+              SizedBox(
+                  height: 30), //put a space between the elems before and after
               Text(
                 "Easily record how much water you drink throughout the day with a tap of a button. No more manual tracking.",
                 textAlign: TextAlign.center,
